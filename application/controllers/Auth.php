a2ca6982ba19d35c69635e1a5a4ef7bba0279f30
alifakbarr <alifakbar8800@gmail.com>
2023-10-04 23:15:21 +0700

    add: pembatasan hak akses user lewat url

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -103,4 +103,9 @@ class Auth extends CI_Controller
         $this->session->set_flashdata('msg', '<div class="alert alert-success" role="alert">Anda telah logout</div>');
         redirect('auth');
     }
+
+    public function blocked()
+    {
+        $this->load->view('auth/blocked');
+    }
 }
