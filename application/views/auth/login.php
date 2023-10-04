91d6a50754be654ccabdad416ad477ae54f362bb
alifakbarr <alifakbar8800@gmail.com>
2023-10-04 14:53:37 +0700

    membuat login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -14,12 +14,16 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Login Page!</h1>
                                 </div>
-                                <form class="user">
+
+                                <?= $this->session->flashdata('msg') ?>
+                                <form class="user" method="post" action="<?= base_url('auth') ?>">
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                        <input type="text" class="form-control form-control-user" id="email" name="email" value="<?= set_value('email') ?>" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                        <?= form_error('email', '<small class="text-danger">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                        <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
                                         Login
